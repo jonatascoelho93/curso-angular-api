60c207b7bef3f274759b65e3f4d92cc5a6361ef7
Jonatas <jonatas.coelho@aluno.faculdadeimpacta.com.br>
2020-06-25 14:37:59 -0300

    Spring initialzr

---
+++ src/main/java/br/com/jonatas/apicursoangular/ApiCursoAngularApplication.java
@@ -0,0 +1,13 @@
+package br.com.jonatas.apicursoangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCursoAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCursoAngularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jonatas/apicursoangular/ApiCursoAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jonatas.apicursoangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCursoAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
